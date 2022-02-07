01814f74ce2fa8c9f6e95326f3f559cb380d1705
AlexandraNHSEI <95365439+AlexandraNHSEI@users.noreply.github.com>
2022-02-07 15:50:15 +0000

    Update codelists (#2)
    * Added shielding list
    
    * Add shielding and vaccination codelists

--- analysis/codelist.py
+++ analysis/codelist.py
@@ -4,3 +4,15 @@ from cohortextractor import codelist_from_csv
 pulse_oximetry_codes = codelist_from_csv(
     "codelists/opensafely-pulse-oximetry.csv", system="snomed", column="code"
 )
+# Add shielding list
+# Shielding list codes from OpenCodelists
+shielding_list = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-shield.csv", system="snomed",
+    column="code")
+
+# First COVID vaccination administration in EMIS
+covid_vaccine_EMIS_codes = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-covadm1.csv",
+  system="snomed",
+  column="code",
+)
