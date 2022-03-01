4184d058d8a12c1d320b4fb7f0c8050177c3c05c
AlexandraNHSEI <alexandra.benson2@nhs.net>
2022-03-01 10:30:52 +0000

    Update vaccination codelists and add care home variable

--- analysis/codelist.py
+++ analysis/codelist.py
@@ -9,9 +9,20 @@ shielding_list = codelist_from_csv(
     "codelists/primis-covid19-vacc-uptake-shield.csv", system="snomed", column="code"
 )
 
+# Care home list
+care_home_codes = codelist_from_csv(
+    "codelists/opensafely-nhs-england-care-homes-residential-status.csv", system="snomed", column="code"
+)
+
 # COVID vaccination administration in EMIS
-covid_vaccine_EMIS_codes = codelist_from_csv(
+covid_vaccine_1_EMIS_codes = codelist_from_csv(
     "codelists/primis-covid19-vacc-uptake-covadm1.csv",
+    system="snomed",
+    column="code",
+)
+
+covid_vaccine_2_EMIS_codes = codelist_from_csv(
+    "codelists/primis-covid19-vacc-uptake-covadm2.csv",
     system="snomed",
     column="code",
 )
