9023b071b404276b51efd64155f83e958d493de2
AlexandraNHSEI <95365439+AlexandraNHSEI@users.noreply.github.com>
2022-03-23 11:48:51 +0000

    Update analysis 1 (#6)
    * Change data processing and date for analysis1
    
    * Correct type hints in analysis_data_processing
    
    * Second correction to type hints

--- analysis/analysis1.py
+++ analysis/analysis1.py
@@ -1,30 +1,15 @@
-# Analysis of outputs
+# Script to create timeseries of pulse oximetry codes usage
 
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+# From .analysis_data_processing import create_population_df
+if "." not in sys.path:
+    sys.path.insert(0, ".")
+from analysis_data_processing import create_population_df
 
-# path to output directory
-dir = "output/"
-# find the input csv files
-filepaths = [f for f in os.listdir(dir) if (f.startswith('input') and f.endswith('.csv'))]
-# append the directory path to filename
-filepaths_dir = [dir + filepath for filepath in filepaths]
-
-# create empty list to append dataframes
-dfs = []
-for file in filepaths_dir:
-    # read in files
-    output=pd.read_csv(file)
-    # Get the index date from the filename
-    index_date = pd.to_datetime(file.split("_", )[1].split(".csv")[0], dayfirst=True)
-    # Add the index date to the file
-    output["index_date"] = index_date
-    # Append the dataframes to the list
-    dfs.append(output)
-
-# Combine all the dataframes together
-population_df = pd.concat(dfs)
+# Create population data frame which includes all weeks and dictionary of cohort size for each individual week
+population_df, cohort_size = create_population_df("output/")
 
 # Create lists of current and required headers
 # Convert pulse oximetry codelist csv into data frame
@@ -41,12 +26,12 @@ oximetry_dictionary = {}
 for n in range(0,len(oximetry_codes_df)):
     oximetry_dictionary[oximetry_codes_headers[n]] = oximetry_headers[n]
 
-#Create data frame of sum totals for each index date for each oximetry code
+# Create data frame of sum totals for each index date for each oximetry code
 oximetry_sum = population_df.groupby(['index_date'], as_index=False)[oximetry_codes_headers].sum()
 # Rename oximetry headers in oximetry sums data frame
 oximetry_sum.rename(columns=oximetry_dictionary,inplace=True)
 # Save the dataframe in outputs folder
-oximetry_sum.to_csv('output/oximetry_sums.csv') 
+oximetry_sum.to_csv('output/table_1_oximetry_counts.csv') 
 
 # Create timeseries of pulse oximetry codes usage
 plot_1 = oximetry_sum.plot.line('index_date',oximetry_headers, figsize=(20, 10), fontsize = 20).get_figure()
@@ -54,4 +39,4 @@ plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
 plt.xlabel("Date", fontsize = 25)
 plt.title('Use of Pulse Oximetry Codes Over Time', fontsize = 40)
 # Save plot in outputs folder
-plot_1.savefig("output/oximetry_timeseries.png", bbox_inches ="tight")
+plot_1.savefig("output/plot_1_oximetry_timeseries.png", bbox_inches ="tight")
