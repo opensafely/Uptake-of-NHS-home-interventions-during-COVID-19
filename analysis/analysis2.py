22c00e3c7f2304630398a73d9d9c33b93e584ded
AlexandraNHSEI <alexandra.benson2@nhs.net>
2022-04-13 11:19:17 +0000

    Add all timeseries breakdowns for specific SNOMED codes

--- analysis/analysis2.py
+++ analysis/analysis2.py
@@ -1,20 +1,28 @@
-# Script to create times series plot of the age categories
+# Script to create times series plot for breakdowns by age category, shielding status, sex, IMD decile, ethnicity and care home residency
 
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import Tuple
+import math
+from textwrap import wrap
 import sys
 if "." not in sys.path:
     sys.path.insert(0, ".")
-from analysis_data_processing import create_population_df, redact_and_round_column, redact_and_round_df
+from analysis_data_processing import create_population_df, redact_and_round_column, redact_and_round_df, code_specific_analysis
+
 
 # Create population data frame which includes all weeks and dictionary of cohort size for each individual week
 population_df, cohort_size = create_population_df("output/")
 
+# Create dictionary of oximetry codes and terms they refer to
+oximetry_codes_df = pd.read_csv('codelists/opensafely-pulse-oximetry.csv')
+oximetry_codes_dict = oximetry_codes_df.set_index("code")["term"].to_dict()
+
 # Define the ranges for each of the age categories
-# (e.g. [-1, 17] will find ages such that -1<age<=17 - age is a whole number in the data frame so this is anyone aged 0 to 17 inclusive)
-age_bins = [-1, 17, 49, 64, 200]
+# (e.g. [-1, 39] will find ages such that -1<age<=39 - age is a whole number in the data frame so this is anyone aged 0 to 40 inclusive)
+age_bins = [-1, 39, 49, 64, 200]
 n = len(age_bins)
 
 # Create list of labels for the different age groups
@@ -27,23 +35,63 @@ age_group_labels.append(f'Age {age_bins[-2]+1} or over')
 
 # Create list of age category for each patient and insert into the population data frame
 age_category = pd.cut(population_df.age, bins = age_bins, labels = age_group_labels)
-population_df.insert(0, 'Age Group', age_category)
+population_df.insert(0, 'age_group', age_category)
+
+# Add IMD Decile column
+population_df['IMD_decile'] = (population_df['index_of_multiple_deprivation']/328.44 * 10).apply(np.ceil).astype(int)
 
 
-# Count the number of patients in each age group for each index date
-age_counts = population_df.groupby(['index_date', 'Age Group']).size().reset_index()
-age_counts.rename(columns={0: 'Counts'},inplace=True)
+# Create timeseries for each of the variables
 
-# Count the denominator (the total number of people) and use to obtain the percentage
-age_counts['denominators'] = age_counts['index_date'].map(cohort_size) 
-age_counts['percentage'] = age_counts['Counts'] / age_counts['denominators'] * 100
-# Save the dataframe in outputs folder
-age_counts.to_csv('output/table_2_age_counts.csv') 
+# Define which oximetry codes to create timeseries for
+codes_of_interest = ['1325191000000108', '1325221000000101', '1325241000000108']
 
-# Plot the counts of the age groups over time (pivot to create separate columns for each age group)
-plot_2_age = age_counts.pivot(index="index_date", columns="Age Group", values="percentage").plot(figsize=(20,10), fontsize = 20).get_figure()
-plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
-plt.xlabel("Date", fontsize = 25)
-plt.ylabel('Percentage of patients', fontsize = 25)
-plt.title('Age groups of patients using pulse oximetry@ home over time', fontsize = 40)
-plot_2_age.savefig("output/plot_2_age_timeseries.png", bbox_inches ="tight")
+# Create timeseries for oximetry codes broken down by sex
+for code in codes_of_interest:
+    code_specific_analysis(code, 'sex', population_df, oximetry_codes_dict)
+    legend_handles, legend_labels = plt.gca().get_legend_handles_labels()
+    new_labels = {'F': 'Female', 'M': 'Male'}
+    for i in range(0, len(legend_labels)):
+        legend_labels[i] = new_labels[legend_labels[i]]
+    plt.legend(legend_labels, loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "sex" + "_timeseries.png", bbox_inches ="tight")
+
+# Create timeseries for oximetry codes broken down by care_home
+for code in codes_of_interest:
+    code_specific_analysis(code, 'care_home', population_df, oximetry_codes_dict)
+    legend_handles, legend_labels = plt.gca().get_legend_handles_labels()
+    new_labels = {'0': 'Not a care home resident', '1': 'Care home resident'}
+    for i in range(0, len(legend_labels)):
+        legend_labels[i] = new_labels[legend_labels[i]]
+    plt.legend(legend_labels, loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + 'care home residency')), fontsize = 40)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "care_home" + "_timeseries.png", bbox_inches ="tight")
+
+# Create timeseries for oximetry codes broken down by ethnicity_6_sus
+for code in codes_of_interest:
+    code_specific_analysis(code, 'ethnicity_6_sus', population_df, oximetry_codes_dict)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + 'ethnicity')), fontsize = 40)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "ethnicity_6_sus" + "_timeseries.png", bbox_inches ="tight")
+
+# Create timeseries for oximetry codes broken down by IMD
+for code in codes_of_interest:
+    code_specific_analysis(code, 'IMD_decile', population_df, oximetry_codes_dict)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + 'IMD decile (1 = most deprived, 10 = least deprived)')), fontsize = 40)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "IMD_decile" + "_timeseries.png", bbox_inches ="tight")
+
+# Create timeseries for oximetry codes broken down by age group
+for code in codes_of_interest:
+    code_specific_analysis(code, 'age_group', population_df, oximetry_codes_dict)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + 'age')), fontsize = 40)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "age_group" + "_timeseries.png", bbox_inches ="tight")
+
+# Create timeseries for oximetry codes broken down by shielding status
+for code in codes_of_interest:
+    code_specific_analysis(code, 'shielding', population_df, oximetry_codes_dict)
+    legend_handles, legend_labels = plt.gca().get_legend_handles_labels()
+    new_labels = {'0': 'Not shielding', '1': 'Shielding'}
+    for i in range(0, len(legend_labels)):
+        legend_labels[i] = new_labels[legend_labels[i]]
+    plt.legend(legend_labels, loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + 'shielding status')), fontsize = 40)
+    plt.savefig("output/timeseries/plot_" + code + "_" + "shielding" + "_timeseries.png", bbox_inches ="tight")
--- analysis/analysis_data_processing.py
+++ analysis/analysis_data_processing.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 from typing import Tuple
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+from textwrap import wrap
 
 # Create population data frame which includes all weeks and dictionary of cohort size for each individual week
 def create_population_df(dir:str = "../output/")-> Tuple[pd.DataFrame, dict]:
@@ -56,3 +59,47 @@ def redact_and_round_df(df: pd.DataFrame) -> pd.DataFrame:
     for column in df.columns.values:
         df[column] = redact_and_round_column(df[column])
     return(df)
+
+
+# Helper funtion for code_specific_analysis function. Takes a dataframe countaining a column of counts and
+# redacts all counts for an index date if any one of the counts for that date is already redacted
+def further_redaction(counts_df: pd.DataFrame) -> pd.DataFrame:
+    # For  each row of the dataframe
+    for index, row in counts_df.iterrows():
+        # If count redacted
+        if row['counts'] == '[REDACTED]':
+            # Find  its index date
+            removal_index = row['index_date']
+            # Redact counts for all rows with that index date
+            counts_df.loc[counts_df["index_date"] == removal_index, "counts"] = '[REDACTED]'
+    return(counts_df)
+
+
+# Function to take a pulse oximetry code and save the timeseries and its underlying table, grouped by a specific column
+def code_specific_analysis(code: str, column_name: str, population_df: pd.DataFrame, oximetry_codes_dict: dict):
+    # Population of interest is all patients with the code
+    codes_df = population_df.loc[population_df['pulse_oximetry_' + code] == 1]
+    # Count the number of patients in each age group for each index date
+    counts_df = codes_df.groupby(['index_date', column_name]).size().reset_index()
+    counts_df.rename(columns={0: 'counts'},inplace=True)
+
+    # Count the denominator (the total size of the cohort for each week)
+    counts_df['denominators'] = counts_df['index_date'].map((codes_df.groupby('index_date').size()).to_dict()) 
+    # Apply redacting and rounding to the counts
+    counts_df['counts'] = redact_and_round_column(counts_df['counts'])
+    # Redact counts for any week where at least one count has been redacted
+    counts_df = further_redaction(counts_df)
+    # Exclude denominators which are less than 100 (higher variation will make timeseries less meaningful)
+    counts_df['denominators'] = np.where(counts_df['denominators'] >= 100, counts_df['denominators'], 'Less than 100')
+    # Calculate the percentages (where possible i.e. when count not redacted and denominator >=100)
+    counts_df['percentage'] = round(pd.to_numeric(counts_df['counts'], errors='coerce') / pd.to_numeric(counts_df['denominators'], errors = 'coerce') * 100, 1)
+
+    # Save the dataframe in outputs folder
+    counts_df.to_csv('output/tables/table_' + code + '_' + column_name + '_counts.csv') 
+
+    # Plot the counts over time (pivot to create separate columns for each grouping)
+    plot_timeseries = counts_df.pivot(index="index_date", columns= column_name, values="percentage").plot(figsize=(20,10), fontsize = 20).get_figure()
+    plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0), fontsize = 20)
+    plt.xlabel("Date", fontsize = 25)
+    plt.ylabel('Percentage of patients', fontsize = 25)
+    plt.title('\n'.join(wrap('Patients with \"' + oximetry_codes_dict[int(code)] + '\" code,\n grouped by ' + column_name)), fontsize = 40)
